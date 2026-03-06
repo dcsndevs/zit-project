1ff260a44e67a7b6feef2f3931b9ea98658db5a6
Dickson <olejemedickson@gmail.com>
2026-03-06 15:14:42 +0000

    Add Serializers for orders and product

---
+++ zit_backend/orders/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from .models import Order, OrderItem
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'created_at', 'total_price', 'items']
+        read_only_fields = ['user']
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        order = Order.objects.create(**validated_data)
+
+        for item in items_data:
+            OrderItem.objects.create(order=order, **item)
+
+        return order
