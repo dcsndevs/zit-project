6be0063571193e91148668f45a6a3b0cf9fff86c
Dickson <olejemedickson@gmail.com>
2026-04-15 13:45:51 +0100

    Increase price size

--- zit_backend/products/models.py
+++ zit_backend/products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
     stock = models.IntegerField()
     image_url = models.URLField(max_length=1000, blank=True, default='')    
 
