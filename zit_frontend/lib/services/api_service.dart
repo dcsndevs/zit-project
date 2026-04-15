40876f4e7ff42270446bd166fc052b3501fe75c3
Dickson <olejemedickson@gmail.com>
2026-04-15 02:37:28 +0100

    Add life url after deployment + set debbug false

--- zit_frontend/lib/services/api_service.dart
+++ zit_frontend/lib/services/api_service.dart
@@ -4,8 +4,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
 
-  static const String baseUrl = 'http://127.0.0.1:8000/api';
-
+  // static const String baseUrl = 'http://127.0.0.1:8000/api';
+  static const String baseUrl = 'http://dcsn.eu.pythonanywhere.com/api';
   // --- AUTH ---
   Future<bool> login(String username, String password) async {
     final response = await http.post(
